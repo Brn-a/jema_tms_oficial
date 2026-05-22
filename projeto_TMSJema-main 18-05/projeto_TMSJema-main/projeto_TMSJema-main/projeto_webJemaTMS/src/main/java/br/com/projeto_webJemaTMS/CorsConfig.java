18eba98cc8b6997aa19e7692dbf39d47cd261d70
Bruno Amaral <bruno.rauf410@gmail.com>
2026-05-22 18:53:42 -0300

    config: adiciona o codigo de fato na classe CorsConfig

--- projeto_TMSJema-main 18-05/projeto_TMSJema-main/projeto_TMSJema-main/projeto_webJemaTMS/src/main/java/br/com/projeto_webJemaTMS/CorsConfig.java
+++ projeto_TMSJema-main 18-05/projeto_TMSJema-main/projeto_TMSJema-main/projeto_webJemaTMS/src/main/java/br/com/projeto_webJemaTMS/CorsConfig.java
@@ -1,5 +1,15 @@
 package br.com.projeto_webJemaTMS;
 
-public class CorsConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173") // Porta padrão do React com Vite
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+    }
 }
